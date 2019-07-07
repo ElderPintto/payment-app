84833cbbe2283a8db2d4979afa2348ab5cebbad8
elderpinto <eldersilvap@gmail>
2019-07-07 14:49:32 +0100

    commit initial

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header container">
+    <div class="row">
+      <div class="header_box">
+        <h3 class="header_subtitle">Subtitle</h3>
+        <h1 class="header_title">Title</h1>
+      </div>
+      <div class="header_box">
+        <figure class="header_profilePhoto profilePhoto">
+          <img clas="profilePhoto_image" src="../assets/profile.jpg" alt="">
+          <figcaption class="profilePhoto_status"></figcaption>
+        </figure>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
